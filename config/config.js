97986556dbd05c45f9defeefb6b02884e47a8288
Warunyoo <warunyoo084@gmail.com>
2025-04-29 17:58:57 +0700

    delete env

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 module.exports = {
   apiKey: process.env.API_KEY || 'your-secret-key', 
   port: process.env.PORT || 3000, 
-  mongoUri: process.env.MONGO_URI || 'mongodb://mongo:f877b6536361f364f133@145.79.10.214:30001/?tls=false', 
+  mongoUri: process.env.MONGO_URI , 
 };
